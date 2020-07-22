233c6b4505247b67ae905ede221264f0c16d5334
KimTK <vodjaos@gmail.com>
2020-07-22 17:05:51 +0900

    2020-07-22

---
+++ BikeShopApp/TestPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Media;
+using BusinessLogic;
+namespace BikeShopApp
+{
+    /// <summary>
+    /// TestPage.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class TestPage : Page
+    {
+        public TestPage()
+        {
+            InitializeComponent();
+            InitListBox();
+        }
+
+        private void InitListBox()
+        {
+            List<Car> lists = new List<Car>();
+            for (int i = 0; i < 10; i++)
+            {
+                lists.Add(new Car()
+                {
+                    Speed = i * 30
+                });
+            }
+            LstCar.DataContext = lists;
+        }
+
+        private void Grid_Loaded(object sender, RoutedEventArgs e)
+        {
+            Car car = new Car();
+            car.Speed = 100;
+            car.Color = Colors.Blue;
+            car.Driver = new Human { Name = "Ted", HasDrivingLicense = true };
+
+            //this.DataContext = car;
+        }
+    }
+}
