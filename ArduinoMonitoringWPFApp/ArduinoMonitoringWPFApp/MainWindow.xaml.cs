ddf2c58eeaa089fdc990f036155da26d05d5cab4
KimTaeKwan <vodjaos@gmail.com>
2020-07-29 19:16:10 +0900

    2020-07-29

---
+++ ArduinoMonitoringWPFApp/ArduinoMonitoringWPFApp/MainWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using MahApps.Metro.Controls;
+using System;
+using System.Linq;
+
+namespace ArduinoMonitoringWPFApp
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+    }
+}
