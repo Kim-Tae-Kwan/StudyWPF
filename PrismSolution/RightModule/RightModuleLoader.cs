18212269bf0ee2b91767ba1f9bb6c495cccff2d0
KimTK <vodjaos@gmail.com>
2020-08-25 17:34:38 +0900

    Prism 20-08-25

---
+++ PrismSolution/RightModule/RightModuleLoader.cs
@@ -0,0 +1,22 @@
+﻿using Prism.Ioc;
+using Prism.Modularity;
+using Prism.Regions;
+using RightModule.Views;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RightModule
+{
+    public class RightModuleLoader : IModule
+    {
+        public void OnInitialized(IContainerProvider containerProvider)
+        {
+            var reionManager = containerProvider.Resolve<IRegionManager>();
+            reionManager.RegisterViewWithRegion("RightRegion", typeof(MessageListView));
+        }
+
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {}
+    }
+}
---
+++ PrismSolution/SecondPrismApp/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Prism.Ioc;
+using Prism.Modularity;
+using Prism.Unity;
+using SecondPrismApp.Views;
+using System.Windows;
+
+namespace SecondPrismApp
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : PrismApplication
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainView>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {}
+
+        protected override void ConfigureModuleCatalog(IModuleCatalog moduleCatalog)
+        {
+            //base.ConfigureModuleCatalog(moduleCatalog);
+            moduleCatalog.AddModule<RightModule.RightModuleLoader>();
+            moduleCatalog.AddModule<LeftModule.ModuleLoader>();
+        }
+    }
+}
